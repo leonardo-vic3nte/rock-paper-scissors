d9c419cb26aec2f3a891e480c8ee70ae42cf568e
Leonardo-Vicente <leonardo.oliveiraa2604@gmail.com>
2023-03-06 12:49:35 -0300

    Add playRound functionality

--- src/app.js
+++ src/app.js
@@ -9,3 +9,33 @@ function getComputerChoice() {
 	const randomIndex = Math.floor(Math.random() * validOutputs.length);
 	return validOutputs[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+	const playerChoice = playerSelection.toLowerCase();
+	const validChoices = [rock, paper, scissors];
+	if (!validChoices.includes(playerChoice)) {
+		return 'invalid';
+	}
+	if (playerChoice === computerSelection) {
+		return 'This round is a tie!';
+	}
+	if (playerSelection === rock) {
+		if (computerSelection === scissors) {
+			return 'You won! Rock beat scissors';
+		}
+		return 'You lost! Rock loses to paper';
+	}
+	if (playerSelection === paper) {
+		if (computerSelection === rock) {
+			return 'You won! Paper beat Rock';
+		}
+		return 'You lost! Paper loses to scissors';
+	}
+	if (playerSelection === scissors) {
+		if (computerSelection === paper) {
+			return 'You win! Scissors beats paper';
+		}
+		return 'You lost! Scissors loses to paper';
+	}
+	return 'Something went wrong...';
+}
